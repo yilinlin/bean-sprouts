39dc914f7f1cb0239b13f255c75d8327fe9b831b
北山秋叶 <asdfqs@163.com>
2020-12-17 13:57:32 +0800

    🎨 sso增加一些补充说明

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -4,7 +4,11 @@
     <div class="dashboard-text">roll your code here ~ by cecloud-fe</div>
 
     <div style="padding: 10px">
-      <span style="margin-left: -10px">sso support: </span>
+      <span style="margin-left: -10px; font-weight: bold;">sso support: </span>
+      <p>具体说明可以参考链接 <a href="http://wiki.cestc.cn:8884/pages/viewpage.action?pageId=41958716" target="_blank" style="color: blue; text-decoration: underline">sso 说明</a></p>
+      <p>原理图:</p>
+      <img src="@/assets/demo/sso.png" width="300" alt="" srcset="">
+      <p>一个示例:</p>
       <div class="dashboard-text"><button @click="checkAuth">check auth(sso) - 401</button></div>
       <div class="dashboard-text"><button @click="setCookie">setCookie</button></div>
     </div>
