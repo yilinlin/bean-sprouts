24db971c8234d225e7f3cd88bc7d20d798c35ae0
北山秋叶 <asdfqs@163.com>
2021-01-21 16:33:29 +0800

    temp

---
+++ src/views/redirect-board/index.vue
@@ -0,0 +1,20 @@
+<!--  三级目录所需的跳板页 -->
+<template>
+  <div>
+    <router-view :key="key" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RedirectBoard',
+  computed: {
+    key() {
+      return this.$route.path
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
